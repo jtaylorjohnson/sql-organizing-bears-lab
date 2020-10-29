e16cc45f2ff79dee4ffdb48992cf104f1a318f25
jtaylorjohnson <jtaylorjohnson@gmail.com>
2020-10-29 15:27:10 -0500

    final

--- lib/create.sql
+++ lib/create.sql
@@ -4,6 +4,6 @@ CREATE TABLE bears (
         age INTEGER,
         gender TEXT,
         color TEXT,
-        temperment TEXT,
-        alive INTEGER,
+        temperament TEXT,
+        alive INTEGER
 );
